963831b6c2a867a21ad167a5853151aa5c708482
Daniel J Blueman <daniel@quora.org>
2022-03-21 13:12:33 +0800

    Use correct test API

--- pinger_test.go
+++ pinger_test.go
@@ -8,33 +8,33 @@ import (
 func Test(t *testing.T) {
    p, err := NewPinger("127.0.0.1", time.Millisecond * 900)
    if err != nil {
-      panic(err)
+      t.Fatal(err)
    }
 
    for i := 0; i < 3; i++ {
       resp, err := p.Ping()
       if err != nil {
-         panic(err)
+         t.Error(err)
       }
 
       if !resp {
-         t.Fatal("localhost didn't ping")
+         t.Error("localhost didn't ping")
       }
    }
 
    p, err = NewPinger("128.0.0.1", time.Millisecond * 900)
    if err != nil {
-      panic(err)
+      t.Fatal(err)
    }
 
    for i := 0; i < 3; i++ {
       resp, err := p.Ping()
       if err != nil {
-         panic(err)
+         t.Error(err)
       }
 
       if resp {
-         t.Fatal("unexpected response")
+         t.Error("unexpected response")
       }
    }
 }
