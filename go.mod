c653c12d9c56a0f079993e6364ede619771c0edf
Daniel J Blueman <daniel@quora.org>
2024-04-26 17:25:53 +0800

    Wrap errors

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/dblueman/pinger
 
 go 1.17
 
-require golang.org/x/net v0.0.0-20220114011407-0dd24b26b47d
+require golang.org/x/net v0.24.0
 
-require golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+require golang.org/x/sys v0.19.0 // indirect
--- pinger.go
+++ pinger.go
@@ -34,7 +34,7 @@ func randShort() (int, error) {
    var b [2]byte
    _, err := rand.Read(b[:])
    if err != nil {
-      return -1, err
+      return -1, fmt.Errorf("randShort: %w", err)
    }
 
    return int(binary.BigEndian.Uint16(b[:])), nil
@@ -44,24 +44,24 @@ func NewPinger(target string, timeout time.Duration) (*Pinger, error) {
    var err error
 
    if conn == nil {
-   	conn, err = icmp.ListenPacket("udp4", "0.0.0.0")
-   	if err != nil {
-   		return nil, err
-   	}
+      conn, err = icmp.ListenPacket("udp4", "0.0.0.0")
+      if err != nil {
+         return nil, fmt.Errorf("NewPinger: %w", err)
+      }
    }
 
-	msg := icmp.Message{
-		Type: ipv4.ICMPTypeEcho,
+   msg := icmp.Message{
+      Type: ipv4.ICMPTypeEcho,
       Code: 0,
-	}
+   }
 
    ips, err := net.LookupIP(target)
    if err != nil {
-      return nil, err
+      return nil, fmt.Errorf("NewPinger: %w", err)
    }
 
    if len(ips) == 0 {
-      return nil, fmt.Errorf("No IP addreses for %s!", target)
+      return nil, fmt.Errorf("no IP addreses for %s", target)
    }
 
    ip := ips[0]
@@ -69,7 +69,7 @@ func NewPinger(target string, timeout time.Duration) (*Pinger, error) {
    udpaddr := net.UDPAddr{IP: ip}
    id, err := randShort()
    if err != nil {
-      return nil, err
+      return nil, fmt.Errorf("NewPinger: %w", err)
    }
 
    return &Pinger{target: ip.String(), udpaddr: udpaddr, timeout: timeout, msg: msg, id: id}, nil
@@ -84,39 +84,39 @@ func (p *Pinger) Ping() (bool, error) {
       // no Body
    }
 
-	wb, err := p.msg.Marshal(nil)
-	if err != nil {
-		return false, err
-	}
-
-	_, err = conn.WriteTo(wb, &p.udpaddr)
+   wb, err := p.msg.Marshal(nil)
    if err != nil {
-		return false, err
-	}
+      return false, fmt.Errorf("Ping: %w", err)
+   }
+
+   _, err = conn.WriteTo(wb, &p.udpaddr)
+   if err != nil {
+      return false, fmt.Errorf("Ping: %w", err)
+   }
 
    err = conn.SetReadDeadline(time.Now().Add(p.timeout))
    if err != nil {
-      return false, err
+      return false, fmt.Errorf("Ping: %w", err)
    }
 
    for {
-   	n, peer, err := conn.ReadFrom(p.buf[:])
-   	if err != nil {
+      n, peer, err := conn.ReadFrom(p.buf[:])
+      if err != nil {
          if os.IsTimeout(err) {
             return false, nil
          }
 
-   		return false, err
-   	}
+         return false, fmt.Errorf("Ping: %w", err)
+      }
 
       if peer.String() != p.target+":0" {
          continue
       }
 
-   	msg, err := icmp.ParseMessage(protocolICMP, p.buf[:n])
-   	if err != nil {
-   		return false, err
-   	}
+      msg, err := icmp.ParseMessage(protocolICMP, p.buf[:n])
+      if err != nil {
+         return false, fmt.Errorf("Ping: %w", err)
+      }
 
       if msg.Type == ipv4.ICMPTypeEchoReply {
          return true, nil
